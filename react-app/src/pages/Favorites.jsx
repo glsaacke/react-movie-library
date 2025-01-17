20bebecfa95a627fb3e33379b1db8637d0a1656f
glsaacke <glsaacke@crimson.ua.edu>
2025-01-16 21:16:28 -0600

    finished

--- react-app/src/pages/Favorites.jsx
+++ react-app/src/pages/Favorites.jsx
@@ -1,6 +1,24 @@
 import '../css/Favorites.css'
+import { useMovieContext } from '../contexts/MovieContext';
+import MovieCard from '../components/MovieCard';
 
 const Favorite = () => {
+    const {favorites} = useMovieContext()
+
+    if (favorites) {
+        return( //FIXME favorites do not stick after refreshing the page
+            <div className='Favorites'>
+                <h2>Your Favorites</h2>
+                <div className="movies-grid">
+                    {favorites.map((movie) => (
+                        <MovieCard movie={movie} key={movie.id} />
+                        )
+                    )}
+                </div>
+            </div>
+        )
+    }
+
     return ( 
         <div className="favorites-empty">
             <h2>No Favorite Movies Yet</h2>
